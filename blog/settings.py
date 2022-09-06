c5dd61e15426608cb5c9c4c137f494c8f9310ed0
andrewdempsey2018 <andrewdempsey2016@gmail.com>
2022-09-06 22:49:40 +0100

    add env variables

--- blog/settings.py
+++ blog/settings.py
@@ -1,19 +1,22 @@
+import environ
 import os
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ajhh69$*ec-+-(bam-ys0t0@^7j43!&!hby+txb6+p#^67zfat'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
