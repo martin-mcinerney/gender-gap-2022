bfb0e5dfbd581625d61151cad903e24384c3b33c
andrewdempsey2018 <andrewdempsey2016@gmail.com>
2022-09-06 23:26:27 +0100

    update db url

--- blog/settings.py
+++ blog/settings.py
@@ -117,3 +117,8 @@ STATICFILES_DIRS = ["static"]
 # Send the user here after they login or logout
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
