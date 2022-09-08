1365ef8b569447bd2194503a3cd43b7128037249
Cy-2-30 (Dee) <lebulicynthia@gmail.com>
2022-09-08 23:44:24 +0000

    added the career link

--- blog/urls.py
+++ blog/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('', include('Posts.urls')),
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    path('career/', views.career, name='career'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -8,3 +8,6 @@ def index(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def career(request):
+    return render(request, 'career.html')
